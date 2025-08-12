5d70ce59b33ad87d8722e09518dc72105962f96c
Sadmaan warshi <spwarshi@gmail.com>
2025-08-13 00:52:39 +0530

    jjj

--- Backend/db.js
+++ Backend/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: 'postgresql://postgres:Sad7004431954@db.mvzxffhbtzqksdgdmbze.supabase.co:5432/postgres',
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
