c6bf403f25f97f9db28c4cb3e23ceffe3171afe6
Sadmaan warshi <spwarshi@gmail.com>
2025-08-13 01:47:25 +0530

    done

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,13 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:Sad7004431954@db.mvzxffhbtzqksdgdmbze.supabase.co:5432/postgres',
-  ssl: {
-    rejectUnauthorized: false
-  }
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
